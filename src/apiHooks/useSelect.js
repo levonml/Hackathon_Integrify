68f0dbfd7c599c237c2d0474f64c91e0c5ed76a4
Levon Stepanyan <levonmll@gmail.com>
2021-11-30 13:10:56 +0200

    fix CORS issue

--- src/apiHooks/useSelect.js
+++ src/apiHooks/useSelect.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 export default function useSelect(name) {
-	let baseUrl = "https://open-api.myhelsinki.fi/v1/";
+	let baseUrl = "/v1/";
 	if (name === "activities") {
 		baseUrl = `${baseUrl}${name}/?limit=55`;
 	} else {
